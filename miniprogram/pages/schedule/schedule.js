299aff3b947aa9b31fd563fb8fb95876eb2d6171
lishuangg <1241982864@qq.com>
2020-04-18 17:20:44 +0800

    update schedule

--- miniprogram/pages/schedule/schedule.js
+++ miniprogram/pages/schedule/schedule.js
@@ -1,5 +1,5 @@
 // pages/schedule/schedule.js
-Component({
+Page({
   /**
    * 组件的属性列表
    */
@@ -17,25 +17,50 @@ Component({
    * 组件的初始数据
    */
   data: {
-    content:'赛程表',
+    inProgress: true,
     newsList:[
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" },
-      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛" }
-    ]
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "done" },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "done" },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "done" },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "done" },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "doing" },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "later" },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "later"  },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "later"  },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "later"  },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "later" },
+      { time: "2020/4/17 8:30", content: "男子甲组100米  预赛", state: "later" }
+    ],
+    tab:0,
+    tabLeft: wx.getSystemInfoSync().windowWidth*0.11,
+    tabWidth: wx.getSystemInfoSync().windowWidth * 0.26
   },
 
+  trigger:function(event) {
+    let currentX = event.touches[0].pageX;
+    let currentLen = currentX - this.data.tabLeft;
+    let moveLen;
+
+    if (currentLen > this.data.tabWidth*2 && currentLen <= this.data.tabWidth*3) {
+      moveLen = this.data.tabWidth * 2;
+    }else if(currentLen > this.data.tabWidth && currentLen <= this.data.tabWidth*2){
+      moveLen = this.data.tabWidth;
+    }else{
+      moveLen = 0;
+    }
+
+    let option = {
+      duration: 300, // 动画执行时间
+      timingFunction: 'ease-in' // 动画执行效果
+    };
+    var animation = wx.createAnimation(option); // 创建动画
+    animation.translateX(0);
+    animation.translateX(moveLen).step();
+    this.setData({
+      animate: animation.export(),// 开始执行动画
+      tab: event.currentTarget.dataset['tab']
+    })
+  },
   /**
  * 生命周期函数--监听页面加载
  */
